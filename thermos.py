d2c3990c1af55a88caa7d880456d279035d18d64
U-TT\michela.lavoie <michela.lavoie@BPRL-CNU0111C5D.tt.local>
2013-03-23 20:41:00 -0400

    Initial commit (I believe this is the latest version right now)

---
+++ thermos.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+from datetime import datetime
+import os
+import configparser
+from time import sleep
+
+# Variables globales
+jours = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
+
+def get_zero():
+    zero = datetime.now()
+    return datetime(zero.year,
+                    zero.month,
+                    zero.day - zero.weekday(),
+                    0,
+                    0)
+
+def get_profils():
+    # Importation des fichiers des configuration
+    profils = dict()
+    for file in os.listdir('profils'):
+        if file[-5:] == '.conf':
+            config = configparser.ConfigParser()
+            config.read('profils/' + file)
+            profils[file[:-5]] = config
+            del(config)
+
+    # Impression des fichiers importés
+#    key_list = list(profils.keys())
+#    key_list.sort()
+#    
+#    for key in key_list:
+#        print(key)
+#        for section in profils[key].sections():
+#            print('    ' + section)
+#            print('         temp = ' + profils[key][section]['temp'])
+#            print('         vent = ' + profils[key][section]['vent'])
+    return profils
+
+def get_horaire(profil):
+    horaire = []
+    zero = get_zero()
+
+    for plage in profils[profil].sections():
+        if not '-' in plage:
+            premier_jour = jours.index(plage.split('.')[0])
+            dernier_jour = jours.index(plage.split('.')[0])
+        else:
+            premier_jour = jours.index(plage.split('-')[0])
+            dernier_jour = jours.index(plage.split('-')[1].split('.')[0])
+        for jour in range(premier_jour, dernier_jour+1):
+            debut = datetime(zero.year,
+                             zero.month,
+                             zero.day + jour,
+                             int(plage.split('.')[1].split(':')[0]),
+                             int(plage.split(':')[1]))
+            delta = debut - zero
+            delta = int(delta.total_seconds())
+            horaire.append((delta,
+                            float(profils[profil][plage]['temp']),
+                            profils[profil][plage]['vent']))
+
+    horaire.sort(reverse=True)
+#    print('(delta, \'temp\', \'vent\')')
+#    print(horaire)
+    return horaire
+
+def get_current():
+    # Détection d'où on se trouve dans l'horaire actuel
+    zero = get_zero()
+    verif = datetime.now()
+#    verif = datetime(2012, 10, 20, 9, 59)
+    delta = verif-zero
+    delta = int(delta.total_seconds()) % 604800
+
+    for plage in horaire:
+        if delta > plage[0]:
+            return plage
+            break
+    else:
+        return horaire[0]
+
+if __name__ == '__main__':
+    profils = get_profils()
+    for profil in profils:
+        horaire = get_horaire(profil)
+        print(profil, get_current())
+
+        # Détection d'où on change de réglage pour toute la semaine
+        for delta in range(0, 604800):
+            for plage in horaire:
+                if (delta >= plage[0]) and (delta-1 < plage[0]):
+                    print(delta, plage)
+                    break
